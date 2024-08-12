b1c60b10c8fb3be891eac6dccc5302fd8c5f52bd
Zach Drake <45634447+thezachdrake@users.noreply.github.com>
2024-08-12 18:45:35 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 df = pd.read_csv("notebooks/data.csv")
 
-table_page = st.Page("pages/table.py", title="Raw Table", icon=":material/add_circle:")
+table_page = st.Page("pages/table.py", title="Raw Table", icon=":material/table:")
 name_page = st.Page("pages/names.py", title="Class Names", icon=":material/add_circle:")
 pg = st.navigation([table_page, name_page])
 st.set_page_config(page_title="Home", page_icon=":material/edit:")
