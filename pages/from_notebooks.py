3221d5be1ef2102890bca9c87a9019999502adc8
Zach Drake <45634447+thezachdrake@users.noreply.github.com>
2024-08-12 19:58:44 -0400

    Update from_notebooks.py

--- pages/from_notebooks.py
+++ pages/from_notebooks.py
@@ -3,6 +3,12 @@ import pandas as pd
 
 df = pd.read_csv("notebooks/data.csv")
 
+# Zach Drake
 st.subheader("Your Name")
+st.text("Tell me about your plot")
+st.divider()
+
+# Sandra Staub
+st.subheader("Sandra Staub")
 st.text("Tell me about your plot")
 st.divider()
