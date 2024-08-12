fd579f8e5ad4079ea6d39d458a23e93ed6985e13
Zach Drake <45634447+thezachdrake@users.noreply.github.com>
2024-08-12 19:38:49 -0400

    Update top_qs.py

--- pages/top_qs.py
+++ pages/top_qs.py
@@ -3,7 +3,8 @@ import pandas as pd
 
 df = pd.read_csv("notebooks/data.csv")
 df = df.sort_values("QS Overall Score", ascending=False).head(15)
-count_df = df.groupby("Location Full").count()
+count_df = df.groupby("Location Full").count()["Size"]
+count_df.rename(columns={"Size":"count"}, inplace=True)
 
 st.header("Top 15 QS Universities")
 # st.bar_chart(df, y="Institution Name", x="QS Overall Score")
