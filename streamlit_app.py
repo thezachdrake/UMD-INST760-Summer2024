726f96744ea25fada7ddd3f153d0c297f6ddcda9
Zach Drake <45634447+thezachdrake@users.noreply.github.com>
2024-08-12 19:54:57 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ import pandas as pd
 df = pd.read_csv("notebooks/data.csv")
 
 top_15_page = st.Page("pages/top_qs.py", title="Top 15", icon=":material/trophy:")
-submissions_page = st.Page("pages/from_notebooks.py", title="Submissions", icons=":material/assignment:")
+submissions_page = st.Page("pages/from_notebooks.py", title="Submissions", icon=":material/assignment:")
 table_page = st.Page("pages/table.py", title="Raw Table", icon=":material/table:")
 name_page = st.Page("pages/names.py", title="Class Roster", icon=":material/groups:")
 pg = st.navigation([top_15_page,submissions_page, table_page, name_page])
