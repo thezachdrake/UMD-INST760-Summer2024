618c598e6ab2c6b2741eec3eaadc42da41fd7274
Zach Drake <45634447+thezachdrake@users.noreply.github.com>
2024-08-12 19:20:00 -0400

    Update top_qs.py

--- pages/top_qs.py
+++ pages/top_qs.py
@@ -2,9 +2,9 @@ import streamlit as st
 import pandas as pd
 
 df = pd.read_csv("notebooks/data.csv")
-df = df.sort_values("QS Overall Score", ascending=False).head(25)
+df = df.sort_values("QS Overall Score", ascending=False).head(15)
 df = df.groupby("Location Full").count()
 
-# st.header("Top 15 QS Universities")
+st.header("Top 15 QS Universities")
 # st.bar_chart(df, y="Institution Name", x="QS Overall Score")
 st.write(df)
