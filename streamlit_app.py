290b6be38b2933b8d0613e275d3b6c3a9ab569e2
Zach Drake <45634447+thezachdrake@users.noreply.github.com>
2024-08-12 18:53:55 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ import pandas as pd
 df = pd.read_csv("notebooks/data.csv")
 
 table_page = st.Page("pages/table.py", title="Raw Table", icon=":material/table:")
-name_page = st.Page("pages/names.py", title="Class Names", icon=":material/groups:")
+name_page = st.Page("pages/names.py", title="Class Roster", icon=":material/groups:")
 pg = st.navigation([table_page, name_page])
 st.set_page_config(page_title="UMD INST 760 - S24", page_icon=":material/edit:")
 pg.run()
