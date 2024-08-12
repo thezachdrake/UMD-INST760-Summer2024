b84e6efc24737cf1a2ef65e3bd8acf10ac6159ed
Zach Drake <45634447+thezachdrake@users.noreply.github.com>
2024-08-12 19:44:09 -0400

    Update top_qs.py

--- pages/top_qs.py
+++ pages/top_qs.py
@@ -5,7 +5,7 @@ df = pd.read_csv("notebooks/data.csv")
 df = df.sort_values("QS Overall Score", ascending=False).head(15)
 count_df = df.groupby("Location Full").count()
 count_df.rename(columns={"Size":"count"}, inplace=True)
-count_df = count_df.sort(ascending=True)
+count_df = count_df.sort_values("count", ascending=True)
 
 st.header("Top 15 QS Universities")
 st.bar_chart(count_df["count"], horizontal=True)
