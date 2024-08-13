bc8e670522f27fcac7dc8d28a38bd65967098b3e
Zach Drake <45634447+thezachdrake@users.noreply.github.com>
2024-08-12 20:09:45 -0400

    Update from_notebooks.py

--- pages/from_notebooks.py
+++ pages/from_notebooks.py
@@ -4,13 +4,13 @@ import pandas as pd
 df = pd.read_csv("notebooks/data.csv")
 
 # Zach Drake
-zd_df = df
+zd_df = df.copy()
 st.subheader("Your Name")
 st.text("Tell me about your plot")
 st.divider()
 
 # Sandra Staub
-ss_df = df
+ss_df = df.copy()
 st.subheader("Sandra Staub")
 st.text("Tell me about your plot")
 st.divider()
