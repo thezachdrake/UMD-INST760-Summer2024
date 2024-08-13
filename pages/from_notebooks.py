fc4fbc31667bd454870303e4b823a15140db5889
Andrea Murano <akmurano91@gmail.com>
2024-08-12 21:48:17 -0400

    adding text desc to heatmap

--- pages/from_notebooks.py
+++ pages/from_notebooks.py
@@ -20,7 +20,11 @@ st.divider()
 # Andrea Murano
 am_df = df.copy()
 st.subheader("Andrea Murano")
-st.text("Tell me about your plot")
+st.markdown("**An Exploration of Metric Correlation:**")
+st.text("The heatmap below displays strong, moderate, and weak correlations between each of the metrics that contributed to the QS score. The metrics that contributed to the QS score include: *Academic Reputation, Employer Reputation, Faculty/Student Ratio, Citations per Faculty, International Faculty Ratio, International Research Network, Employment Outcomes, and Sustainability*.
+Strong Positive Correlations: Academic Reputation and Employer Reputation are strongly correlated with each other. Possibly unsurprisingly, there is a strong positive correlation between International Students and International Faculty.
+Moderately Positive Correlations: Citations per Faculty member has moderately positive correlations with Academic Reputation and International Research Network. Sustainability shows a moderately positive correlation to Academic Reputation and International Research Network.
+Weak Correlation: Overall, Faculty Student Ratio shows the weakest correlation across the board to other metrics.")
 am_scored_columns = df.select_dtypes(include=['float64'])
 am_score_matrix = am_scored_columns.corr()
 fig, ax = plt.subplots(figsize=(10,10))
