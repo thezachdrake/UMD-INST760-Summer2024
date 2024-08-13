ad890852c12b1dc34505d33326dd7950f62d723b
Andrea Murano <akmurano91@gmail.com>
2024-08-12 21:27:36 -0400

    added sns, plt, heatmap

--- pages/from_notebooks.py
+++ pages/from_notebooks.py
@@ -1,5 +1,7 @@
 import streamlit as st
 import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 df = pd.read_csv("notebooks/data.csv")
 
@@ -21,5 +23,7 @@ st.subheader("Andrea Murano")
 st.text("Tell me about your plot")
 am_scored_columns = df.select_dtypes(include=['float64'])
 am_score_matrix = am_scored_columns.corr()
-st.write(am_score_matrix)
+fig, ax = plt.subplots(figsize=(10,10))
+sns.heatmap(am_score_matrix, ax=ax, cmap="YlOrRd", annot=True)
+st.pyplot(fig)
 st.divider()
