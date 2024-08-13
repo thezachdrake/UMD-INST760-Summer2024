94c4dbafeb36c2402e771c00bb6f1dd35c49e140
Andrea Murano <akmurano91@gmail.com>
2024-08-13 13:59:47 -0400

    adding a separate block of code to try out new way to visualize data

--- pages/top_qs.py
+++ pages/top_qs.py
@@ -9,3 +9,14 @@ count_df = count_df.sort_values("count", ascending=False)
 
 st.header("Top 15 QS Universities")
 st.bar_chart(count_df["count"], horizontal=True)
+
+st.divider()
+#Trying out a different way to visualize this data - Andrea Murano
+df = pd.read_csv("notebooks/data.csv")
+df = df.sort_values("QS Overall Score", ascending=False).head(15)
+count_df = df.groupby("Location Full").count()
+count_df.rename(columns={"Size":"count"}, inplace=True)
+count_df = count_df.sort_values("count", ascending=True)
+
+st.header("Top 15 QS Universities")
+st.bar_chart(count_df["count"])
