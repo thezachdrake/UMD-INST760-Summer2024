dac68abdb9cb413e281d6f3c7815829eba4764dc
Andrea Murano <akmurano91@gmail.com>
2024-08-13 14:11:50 -0400

    Update top_qs.py

--- pages/top_qs.py
+++ pages/top_qs.py
@@ -1,7 +1,5 @@
 import streamlit as st
 import pandas as pd
-pip install --upgrade plotly
-import plotly.express as px
 
 df = pd.read_csv("notebooks/data.csv")
 df = df.sort_values("QS Overall Score", ascending=False).head(15)
@@ -12,7 +10,3 @@ count_df = count_df.sort_values("count", ascending=False)
 st.header("Top 15 QS Universities")
 st.bar_chart(count_df["count"], horizontal=True)
 
-st.divider()
-#Trying out a different way to visualize this data - Andrea Murano
-fig = px.bar(count_df, x=count_df.index, y="count", labels={'x':'Location', 'y':'count'})
-st.plotly_chart(fig)
