c66c9275d754621b7b74019eeaa9645cead22693
Andrea Murano <akmurano91@gmail.com>
2024-08-12 22:58:54 -0400

    update axes

--- pages/from_notebooks.py
+++ pages/from_notebooks.py
@@ -28,12 +28,17 @@ am_int_schools = am_df['Location'] != 'US'
 
 fig, axs = plt.subplots(2, 2, figsize=(10, 10))
 sns.regplot(x='Employment Outcomes', y='Academic Reputation', data=am_df[am_US_schools], ax=axs[0, 0])
+axs[0, 0].set_xlabel(' ')
 axs[0, 0].set_ylabel('Academic Reputation')
 sns.regplot(x='Employment Outcomes', y='Academic Reputation', data=am_df[am_int_schools], ax=axs[0, 1])
+axs[0, 1].set_xlabel(' ')
+axs[0, 1].set_ylabel(' ')
 sns.regplot(x='Employment Outcomes', y='Employer Reputation', data=am_df[am_US_schools], ax=axs[1, 0])
 axs[1, 0].set_xlabel('Employment Outcomes')
 axs[1, 0].set_ylabel('Employer Reputation')
 sns.regplot(x='Employment Outcomes', y='Employer Reputation', data=am_df[am_int_schools], ax=axs[1, 1])
+axs[1, 1].set_xlabel(' ')
+axs[1, 1].set_ylabel(' ')
 
 plt.subplots_adjust(wspace=0.3, hspace=0.3)
 st.pyplot(fig)
