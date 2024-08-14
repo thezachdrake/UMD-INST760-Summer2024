d2a6ad02a4e0466834caf2f4bf60d77e6ffa1813
ivyrobertsis <irobert6@umd.edu>
2024-08-14 03:54:20 -0400

    testing IR

--- pages/from_notebooks.py
+++ pages/from_notebooks.py
@@ -17,6 +17,21 @@ st.subheader("Sandra Staub")
 st.text("Tell me about your plot")
 st.divider()
 
+# Ivy Roberts
+ir_df = df.copy()
+st.subheader("Ivy Roberts")
+st.write("In this plot, I discovered from the data that our mystery student will be happiest at a top college in France. This plot shows the top schools in France that align with her ideals.")
+#New dataset for all schools in France
+France = df[df.Location=="FR"]
+France
+sns.catplot(x="institution_name", y="2025_rank", data=France, kind="bar")
+plt.xlim(0, 3)
+plt.xticks(rotation=40)
+plt.yticks(rotation=0)
+plt.show()
+st.pyplot(fig)
+st.divider()
+
 # Andrea Murano
 am_df = df.copy()
 st.subheader("Andrea Murano")
@@ -50,21 +65,6 @@ fig.text(0.07, 0.71, 'Academic Reputation', ha='center', va='center', rotation='
 fig.text(0.07, 0.29, 'Employer Reputation', ha='center', va='center', rotation='vertical', fontsize=15)
 
 plt.subplots_adjust(wspace=0.3, hspace=0.4)
-st.pyplot(fig)
-st.divider()
-
-# Ivy Roberts
-ir_df = df.copy()
-st.subheader("Ivy Roberts")
-st.write("In this plot, I discovered from the data that our mystery student will be happiest at a top college in France. This plot shows the top schools in France that align with her ideals.")
-#New dataset for all schools in France
-France = df[df.Location=="FR"]
-France
-sns.catplot(x="institution_name", y="2025_rank", data=France, kind="bar")
-plt.xlim(0, 3)
-plt.xticks(rotation=90)
-plt.yticks(rotation=0)
-plt.show()
 st.pyplot(fig)
 st.divider()
 
