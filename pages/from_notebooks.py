cf2b2898439b87f5e737f77ebde8e22ac2cf163b
S. Richardson <157779421+neekrich@users.noreply.github.com>
2024-08-15 14:37:42 -0400

    Updating SR

--- pages/from_notebooks.py
+++ pages/from_notebooks.py
@@ -107,7 +107,7 @@ sr_df = df.copy()
 st.subheader("Shanikka Richardson")
 st.write("Tell me about your plot")
 
-top_schools = df.head(25)
+top_schools = sr_df.head(25)
 plt.figure(figsize=(10, 6))
 sns.scatterplot(data=top_schools, x='employer_reputation', y='employment_outcomes', hue='institution_name')
 
