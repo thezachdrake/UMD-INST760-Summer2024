88474213a6b2f54a34b3105f652d71dd7c90da6c
sstaub1@umd.edu <117780688+sstaub1@users.noreply.github.com>
2024-08-18 19:57:38 -0400

    Update from_notebooks.py

--- pages/from_notebooks.py
+++ pages/from_notebooks.py
@@ -253,7 +253,7 @@ st.subheader("Sandra Staub")
 st.text("Tell me about your plot")
 df_us = df[df['location'] == 'US']
 sns.set(style="whitegrid")
-sns.scatterplot(x="employer_reputation", y="employment_outcomes", data=df_us, hue="size")
+sns.scatterplot(x="employer eputation", y="employment outcomes", data=df_us, hue="size")
 # Add a title using matplotlib
 plt.title = plt.title
 plt.title("US Schools")
