ca4062730928c9b393b86b0c2e66a307e01d5188
sstaub1@umd.edu <117780688+sstaub1@users.noreply.github.com>
2024-08-19 14:41:52 -0400

    Update from_notebooks.py

--- pages/from_notebooks.py
+++ pages/from_notebooks.py
@@ -326,6 +326,38 @@ for index, row in points_to_label.iterrows():
 
 plt.title("US Schools Employer Stats")
 st.pyplot(plt)
+
+st.write("which schools are these in the upper write corner?")
+
+import plotly.express as px 
+# Filter data
+ss_df_us = ss_df[(ss_df['Location'] == 'US') & 
+           (ss_df['Employer Reputation'] > 90) & 
+           (ss_df['Employment Outcomes'] > 90)]
+
+# Create a scatter plot with Plotly
+fig = px.scatter(ss_df_us, 
+                 x="Employer Reputation", 
+                 y="Employment Outcomes", 
+                 color="size", 
+                 text="Institution Name",
+                 hover_data={
+                     'Employer Reputation': True, 
+                     'Employment Outcomes': True,
+                     'Institution Name': True
+                 })
+
+# Customize hover mode
+fig.update_traces(marker=dict(size=12),
+                  textposition='top center',
+                  hovertemplate="<b>%{text}</b><br>Employer Reputation: %{x}<br>Employment Outcomes: %{y}<extra></extra>")
+
+# Add a title
+fig.update_layout(title="US Schools")
+
+# Display the plot in Streamlit (or Plotly directly)
+fig.show()
+st.plotly_chart(fig)
 st.divider()
 
 # Victoria Nathaniel
