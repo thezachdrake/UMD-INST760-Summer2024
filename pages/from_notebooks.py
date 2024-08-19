c7356fd22e68b94d3eb6eb53b953e3adb52fb52c
Andrea Murano <akmurano91@gmail.com>
2024-08-18 22:04:09 -0400

    Update from_notebooks.py

--- pages/from_notebooks.py
+++ pages/from_notebooks.py
@@ -51,7 +51,6 @@ emp_outcome_corr('Employment Outcomes', 'Employer Reputation', am_df[am_US_schoo
 emp_outcome_corr('Employment Outcomes', 'Employer Reputation', am_df[am_int_schools], axs[1, 1], 
                   'International Schools - Employer Reputation', 'indigo')
 
-fig.tight_layout(pad=3.0)
 plt.subplots_adjust(wspace=0.3, hspace=0.4)
 
 cbar_ax = fig.add_axes([0.15, 0.05, 0.7, 0.02])
