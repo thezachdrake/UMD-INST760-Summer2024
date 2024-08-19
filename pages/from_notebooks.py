06e936f08af3c1d597c90b3cb2b4d0afa95691c8
vRia4 <139405100+vRia4@users.noreply.github.com>
2024-08-18 23:10:02 -0400

    Update from_notebooks.py

--- pages/from_notebooks.py
+++ pages/from_notebooks.py
@@ -296,7 +296,7 @@ vn_df = df.copy()
 st.subheader("Victoria Nathaniel")
 st.write("Showing the academic reputation, and employment outcomes in USA")
 plt.figure(figsize=(10, 6))
-location_in_us = vn_df[vn_df['location'] == 'US']
+location_in_us = vn_df['location'] == 'US'
 sns.scatterplot(data=location_in_us, x='employment_outcomes', y='academic_reputation', hue='size', palette='Set2', alpha=0.7, ax=ax)
 plt.xlabel('Percentage Employment')
 plt.ylabel('Academic Reputation')
