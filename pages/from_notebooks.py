a8da10a6a9b06c7eae3bb600fc62f82f743cb119
sstaub1@umd.edu <117780688+sstaub1@users.noreply.github.com>
2024-08-19 22:23:05 -0400

    Update from_notebooks.py

--- pages/from_notebooks.py
+++ pages/from_notebooks.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
-import plotly.express as px
+#import plotly.express as px
 
 
 df = pd.read_csv("notebooks/data.csv")
