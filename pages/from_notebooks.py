4c007571920377642f2ab48fefd7cdbffa0b5f74
sstaub1@umd.edu <117780688+sstaub1@users.noreply.github.com>
2024-08-19 22:21:10 -0400

    Update from_notebooks.py

--- pages/from_notebooks.py
+++ pages/from_notebooks.py
@@ -2,7 +2,6 @@ import streamlit as st
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
-pip install streamlit plotly
 import plotly.express as px
 
 
@@ -330,32 +329,32 @@ plt.title("US Schools Employer Stats")
 st.plotly(fig)
 st.write("which schools are these in the upper right corner?")
 #Filter data
-#ss_df_us = ss_df[(ss_df['Location'] == 'US') & 
- #         (ss_df['Employer Reputation'] > 90) & 
-  #       (ss_df['Employment Outcomes'] > 90)]
+ss_df_us = ss_df[(ss_df['Location'] == 'US') & 
+         (ss_df['Employer Reputation'] > 90) & 
+       (ss_df['Employment Outcomes'] > 90)]
 
  # Create a scatter plot with Plotly
-#fig = px.scatter(ss_df_us, 
-#                 x="Employer Reputation", 
- #                y="Employment Outcomes", 
-#                 color="size", 
-#                text="Institution Name",
-#                 hover_data={
-#                     'Employer Reputation': True, 
-#                     'Employment Outcomes': True,
-#                     'Institution Name': True
-#                })
+fig = px.scatter(ss_df_us, 
+                 x="Employer Reputation", 
+                y="Employment Outcomes", 
+                 color="size", 
+                text="Institution Name",
+                 hover_data={
+                     'Employer Reputation': True, 
+                    'Employment Outcomes': True,
+                     'Institution Name': True
+                 })
 
 # Customize hover mode
-#fig.update_traces(marker=dict(size=12),
-#                textposition='top center',
-#                hovertemplate="<b>%{text}</b><br>Employer Reputation: %{x}<br>Employment Outcomes: %{y}<extra></extra>")
+ fig.update_traces(marker=dict(size=12),
+                 textposition='top center',
+                hovertemplate="<b>%{text}</b><br>Employer Reputation: %{x}<br>Employment Outcomes: %{y}<extra></extra>")
 
 # Add a title
-#fig.update_layout(title="US Schools")
+ fig.update_layout(title="US Schools")
 
 # Display the plot
-#st.plotly_chart(fig)
+ st.plotly_chart(fig)
 st.divider()
 
 # Victoria Nathaniel
